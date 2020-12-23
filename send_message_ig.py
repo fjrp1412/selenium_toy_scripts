e246a47d3db294fd2572ac3d931e8acbfaf98a98
Frnacisco <fruiz.rc@gmail.com>
2020-12-22 22:48:31 -0400

    Arreglado mini bug

--- send_message_ig.py
+++ send_message_ig.py
@@ -44,7 +44,7 @@ class InstagramMessage(unittest.TestCase):
         contact = ""
 
         for i in range(1,11):
-            name = driver.find_element_by_xpath(f'/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[{i}]/a/div/div[2]/div[1]           /div/div/div/div').text
+            name = driver.find_element_by_xpath(f'/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[{i}]/a/div/div[2]/div[1]/div/div/div/div').text
             if(name == contact_to_search):
                 contact = driver.find_element_by_xpath(f'/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[{i}]/a')
                 break
@@ -60,7 +60,7 @@ class InstagramMessage(unittest.TestCase):
         message = input("Insert a message to sent:  ")
         text_field.send_keys(message)
 
-        send_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable( (By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div       [2]/div/div[2]/div/div/div[3]/button')))
+        send_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable( (By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button')))
         send_button.click()
         time.sleep(10)
 
