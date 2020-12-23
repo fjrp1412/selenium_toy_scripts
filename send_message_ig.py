bba0bd4819f77226044aaa92bba53d2a7ae925dd
Frnacisco <fruiz.rc@gmail.com>
2020-12-22 21:42:16 -0400

    Change comments to english from spanish

--- send_message_ig.py
+++ send_message_ig.py
@@ -14,8 +14,8 @@ class InstagramMessage(unittest.TestCase):
 
     def enterToAccount(self):
         driver = self.driver
-        USER = 'franjrp_'
-        PASSWORD = 'crashvarius1'
+        USER = '' #Insert your user account
+        PASSWORD = '' #Insert your password
 
         user_field = WebDriverWait(driver, 15).until(EC.visibility_of_element_located( (By.NAME, 'username')))
         user_field.clear()
@@ -40,7 +40,7 @@ class InstagramMessage(unittest.TestCase):
         instagram_direct = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[2]/a/div/div/div')
         instagram_direct.click()
 
-        contact_to_search = input("A que contacto buscas? ")
+        contact_to_search = input('Select the name of a contact to send a message: ')
         contact = ""
 
         for i in range(1,11):
@@ -57,7 +57,7 @@ class InstagramMessage(unittest.TestCase):
         driver = self.driver
         text_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located( ( By.TAG_NAME, 'textarea')))
         text_field.clear()
-        message = input("Inserte el mensaje a enviar: ")
+        message = input("Insert a message to sent:  ")
         text_field.send_keys(message)
 
         send_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable( (By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div       [2]/div/div[2]/div/div/div[3]/button')))
